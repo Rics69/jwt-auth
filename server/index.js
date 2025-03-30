39b2bb4d1d7369899cee1979d7434a1a03727f5e
Rics <danil.klementev.29@gmail.com>
2025-03-30 19:17:06 +0300

    feat: add server base app

--- server/index.js
+++ server/index.js
@@ -2,5 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 
+const PORT = 5000
 const app = express()
 
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Сервер запущен на порте - ${PORT}`))
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+start()
