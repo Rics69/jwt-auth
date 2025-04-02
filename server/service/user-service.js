edfc1c00b07334f6260ba5eb4e076fceed668501
Rics <danil.klementev.29@gmail.com>
2025-04-02 22:21:04 +0300

    feat: add registration function in service

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,5 +1,19 @@
-class UserService {
+const UserModel = require('../model/user-model')
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+const mailService = require('./mail-service')
 
+class UserService {
+    async registration(email, password) {
+        const canditate = await UserModel.findOne({email})
+        if (canditate) {
+            throw new Error(`Пользователь с почтовым адресом ${email} уже существует!`)
+        }
+
+        const hashPassword = await bcrypt.hash(password, 3)
+        const activationLink = uuid.v4()
+        const user = await UserModel.create({email, password: hashPassword, activationLink})
+    }
 }
 
 module.exports = new UserService()
