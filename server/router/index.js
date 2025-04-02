8c0e88b00dd6b84ea29bad4442792af00b564bc2
Rics <danil.klementev.29@gmail.com>
2025-04-02 22:10:58 +0300

    feat: add controller and services

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,13 @@
-const Router = require('express').Router();
+const Router = require('express');
+const userController = require('../controller/user-controller')
 
-const router = new Router();
+const router = Router.Router();
 
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
-router.get('/refresh');
-router.get('/users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
+
+module.exports = router
