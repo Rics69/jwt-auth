01e329b625d9bb136844afe99e1153f7ec13aa98
Rics <danil.klementev.29@gmail.com>
2025-04-03 14:12:56 +0300

    feat: add mail sender and token generate

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -2,6 +2,7 @@ const UserModel = require('../model/user-model')
 const bcrypt = require('bcrypt')
 const uuid = require('uuid')
 const mailService = require('./mail-service')
+const tokenService = require('./token-service')
 
 class UserService {
     async registration(email, password) {
@@ -13,6 +14,8 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3)
         const activationLink = uuid.v4()
         const user = await UserModel.create({email, password: hashPassword, activationLink})
+        await mailService.sendActivationMail(email, activationLink)
+        const tokens = tokenService.generateTokens()
     }
 }
 
