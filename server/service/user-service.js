8ea55019a0170325244bf218f07b4888a249210f
Rics <danil.klementev.29@gmail.com>
2025-04-04 14:43:34 +0300

    Feat: add activate link function

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -27,6 +27,16 @@ class UserService {
             user: userDto
         }
     }
+
+    async activate(activationLink){
+        const user = await UserModel.findOne({activationLink})
+        if (!user) {
+            throw new Error('Неккоректная ссылка активации')
+        }
+
+        user.isActivated = true;
+        await user.save()
+    }
 }
 
 module.exports = new UserService()
